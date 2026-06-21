72375e32d12482fcdeeec7f26dbd374a30ba9b03
abhigyanchetri-hue <abhigyanchetri@gmail.com>
2026-06-21 22:03:41 +0530

    dsa

---
+++ Starting_Dsa/BASICS.cpp/arrays/strings.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string str1 = "abhi";
+    string str2="gyan";
+
+    cout<<str1+str2<<endl;
+    cout<<str1.length();
+    return 0;
+}
