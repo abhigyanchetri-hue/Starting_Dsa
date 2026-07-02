a0df58f810120cc13d2298bbb64313c0db4f337f
abhigyanchetri-hue <abhigyanchetri@gmail.com>
2026-07-03 01:57:59 +0530

    array

--- Starting_Dsa/BASICS.cpp/StriverSheet/arrays/union.cpp
+++ Starting_Dsa/BASICS.cpp/StriverSheet/arrays/union.cpp
@@ -1,6 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//Brute
+
 vector <int> sorted_array(vector <int>& arr1, int n, vector<int>& arr2, int m){
     set<int> st;
     for (int i=0; i<n; i++){
@@ -30,4 +32,56 @@ int main(){
     for (int i=0; i<result.size(); i++){
         cout<<result[i]<<" ";
     }
+}
+
+//Optimal
+
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> sorted_union(vector<int>& a1, int n1, vector <int>& a2, int n2){
+    int i = 0;
+    int j = 0;
+    vector<int> unionArr;
+    while(i<n1 && j<n2){
+        if (a1[i]<=a2[j]){
+            if (unionArr.size()==0 || unionArr.back()!=a1[i]){
+                unionArr.push_back(a1[i]);
+            }i++;
+        
+        }
+        else if (a1[i]>=a2[j]){
+            if (unionArr.size()==0 || unionArr.back()!=a2[j]){
+                unionArr.push_back(a2[j]);
+            }j++;
+        }
+    }
+    while(i<n1){
+        if (unionArr.size()==0 || unionArr.back()!=a1[i]){
+            unionArr.push_back(a1[i]);
+        }i++;
+    }
+    while(j<n2){
+        if (unionArr.size()==0 || unionArr.back()!=a2[j]){
+            unionArr.push_back(a2[j]);
+        }j++;
+    }
+    return unionArr;
+}
+
+int main(){
+    int n1,n2;
+    cin>>n1>>n2;
+    vector<int> a1(n1),a2(n2);
+    for(int i=0; i<n1;i++){
+        cin>>a1[i];
+    }
+    for(int i=0; i<n2;i++){
+        cin>>a2[i];
+    }
+    vector<int> result=sorted_union(a1,n1,a2,n2);
+    for (int i =0; i<result.size(); i++){
+        cout<<result[i]<<" ";
+    }
+    return 0;
 }
