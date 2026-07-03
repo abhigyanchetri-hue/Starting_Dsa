5a07d9094c444bf37a105c4b62976f55228bde60
abhigyanchetri-hue <abhigyanchetri@gmail.com>
2026-07-04 00:35:51 +0530

    array

--- Starting_Dsa/BASICS.cpp/StriverSheet/arrays/number_appearing_once.cpp
+++ Starting_Dsa/BASICS.cpp/StriverSheet/arrays/number_appearing_once.cpp
@@ -25,7 +25,7 @@ int main(){
     return 0;
 }
 
-//Better
+//Better - 1
 
 #include<bits/stdc++.h>
 using namespace std;
@@ -56,6 +56,30 @@ int main(){
     }
 }
 
+//Better - 2
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for (int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    map <long long,int> mpp;
+    for (int i = 0; i<n; i++){
+        mpp[arr[i]]++;
+    }
+    for (auto it : mpp){
+        if (it.second==1){
+            cout<<it.first;
+        }
+    }
+    return 0;
+}
+
 //Optimal
 
 #include<bits/stdc++.h>
@@ -68,5 +92,9 @@ int main(){
     for (int i=0; i<n; i++){
         cin>>arr[i];
     }
+    int xor1=0;
+    for (int i=0; i<n; i++){
+        xor1=xor1^arr[i];
+    }cout<<xor1;
     return 0;
 }
