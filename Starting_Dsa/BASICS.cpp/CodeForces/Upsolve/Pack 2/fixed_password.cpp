4aa6fdc1fc71027ff576be6d297ff09e0e29fc46
abhigyanchetri-hue <abhigyanchetri@gmail.com>
2026-07-12 10:19:46 +0530

    upsolve it

---
+++ Starting_Dsa/BASICS.cpp/CodeForces/Upsolve/Pack 2/fixed_password.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    bool b = true;
+    while (true){
+        int x;
+        cin>>x;
+        if (x==1999){
+            cout<<"Correct"<<endl;
+            break;
+        }else {
+            cout<<"Wrong"<<endl;
+        }
+    }
+    return 0;
+}
