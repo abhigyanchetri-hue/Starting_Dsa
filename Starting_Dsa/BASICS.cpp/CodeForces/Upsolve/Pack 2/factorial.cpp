fda96fe5296233c79af02b2730849fb8a186a6a2
abhigyanchetri-hue <abhigyanchetri@gmail.com>
2026-07-15 00:15:14 +0530

    upsolve it

--- Starting_Dsa/BASICS.cpp/CodeForces/Upsolve/Pack 2/factorial.cpp
+++ Starting_Dsa/BASICS.cpp/CodeForces/Upsolve/Pack 2/factorial.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 int main(){
-    int n;
+    long long n;
     cin>>n;
-    for (int i=0 ;i<n; i++){
-        int sum = 1;
-        int x;
+    while (n--){
+        long long sum = 1;
+        long long x;
         cin>>x;
-        for (int j=1; i<=x; i++){
+        for (long long i=1; i<=x; i++){
             sum*=i;
         }cout<<sum<<endl;
     }
