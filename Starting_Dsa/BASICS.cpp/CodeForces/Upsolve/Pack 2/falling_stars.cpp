7496bfca9cf1b008c7f11ce6aa92ca3063dff7e2
abhigyanchetri-hue <abhigyanchetri@gmail.com>
2026-07-16 00:04:10 +0530

    upsolve it

--- Starting_Dsa/BASICS.cpp/CodeForces/Upsolve/Pack 2/falling_stars.cpp
+++ Starting_Dsa/BASICS.cpp/CodeForces/Upsolve/Pack 2/falling_stars.cpp
@@ -5,7 +5,9 @@ int main(){
     int n;
     cin>>n;
     for (int i=0; i<n; i++){
-        for (int j=n-i; j<; j--)
+        for (int j=0; j<n-i; j++){
+            cout<<"*";
+        }cout<<endl;
     }
     return 0;
 }
